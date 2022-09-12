8382117f8174233409a10712ad858b4bafbf6ef0
chobela <chobelak@gmail.com>
2022-09-13 01:32:39 +0200

    Compress File

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const multer = require("multer");
 const path = require("path");
+const { exec } = require("child_process");
+const uuid = require("uuid");
 
 const app = express();
 
@@ -11,7 +13,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+    cb(null, uuid.v4() + path.extname(file.originalname)); //Appending extension
   },
 });
 
@@ -35,6 +37,23 @@ app.get("/", (req, res) => {
   res.send("Welcome to Video Uploads");
 });
 
+// app.get("/convert", (req, res) => {
+//   exec(
+//     "ffmpeg -i uploads/36efe2ad-5d3d-4eb6-8a9d-37af717124d1.mp4 -vcodec libx265 -crf 28 compressed/36efe2ad-5d3d-4eb6-8a9d-37af717124d1.mp4",
+//     (error, stdout, stderr) => {
+//       if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//       }
+//       if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//       }
+//       console.log(`stdout: ${stdout}`);
+//     }
+//   );
+// });
+
 // app.post("/upload", (req, res) => {
 //   upload(req, res, (err) => {
 //     if (err) {
@@ -48,4 +67,27 @@ app.post("/upload", upload.single("myfile"), function (req, res) {
   // req.body will hold the text fields, if there were any
 
   console.log(req.file);
+
+  const path = req.file.path;
+  const compressedPath = "compressed/" + req.file.filename;
+
+  exec(
+    "ffmpeg -i " + path + " -vcodec libx265 -crf 28 " + compressedPath,
+    (error, stdout, stderr) => {
+      if (error) {
+        //console.log(`error: ${error.message}`);
+        res.send(`${req.protocol}://${req.get("host")}/${compressedPath}`);
+        return;
+      }
+      if (stderr) {
+        //console.log(`stderr: ${stderr}`);
+        //console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+        res.send(`${req.protocol}://${req.get("host")}/${compressedPath}`);
+        return;
+      }
+      //console.log(`stdout: ${stdout}`);
+      // console.log(`${req.protocol}://${req.get("host")}${req.originalUrl}`);
+      res.send(`${req.protocol}://${req.get("host")}/${compressedPath}`);
+    }
+  );
 });
